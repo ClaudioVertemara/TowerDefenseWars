fb7a4cc6a78b49bdb6f48c33e04d4f385d752abf
Claudio <46950170+ClaudioVertemara@users.noreply.github.com>
2020-11-04 14:21:03 -0800

    Added Start Prototype

---
+++ Tower Defense Wars/Assets/ClaudioV/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject selection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Clicked(GameObject obj) {
+        if (obj.name == "Background") {
+            selection.SetActive(false);
+        } else if (obj.CompareTag("Blue")) {
+            selection.SetActive(true);
+            selection.transform.position = obj.transform.position;
+        }
+    }
+}
