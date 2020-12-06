ccef5bdaf8fa780219c4a7a6ea58e0207f66bc64
MasonEmura <memura@csumb.edu>
2020-12-06 13:41:45 -0800

    Added targeting system
    Allows enemy to target a tower to attack

--- Tower Defense Wars/Assets/MasonE/Scripts/Enemy.cs
+++ Tower Defense Wars/Assets/MasonE/Scripts/Enemy.cs
@@ -4,60 +4,86 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    int health;
-    int damage;
-    string type;
+    Text troopAmountText;
+    int enemyAmount;
+    string enemyType;
     float speed;
 
+    GameObject toTower;
+
+    GameObject goingToTower;
+    GameObject Tower;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        troopAmountText = transform.GetChild(0).GetComponent<Text>();
+
+        enemyAmount = 0;
+        enemyType = "F";
+        speed = 0.2f;
+    }
+
+    // Called when Troops are Spawned, Sets Troops Info Up
+    public void SpawnTroops(int enemyAmount, string enemyType, GameObject toTower)
+    {
+        this.enemyAmount = enemyAmount;
+        troopAmountText.text = enemyAmount.ToString();
+
+        this.enemyType = enemyType;
+        SetSpeed();
+
+        this.toTower = toTower;
+
+        goingToTower = TargetTower();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, goingToTower.transform.position, speed * Time.deltaTime);
+
+        // Troops Arrived at Tower
+        if (transform.position == toTower.transform.position)
+        {
+            Tower tower = toTower.GetComponent<Tower>();
+
+            if (toTower.CompareTag("Blue"))
+            {
+                tower.ChangeTroopAmount(enemyAmount, enemyType, true);
+            }
+            else
+            {
+                tower.ChangeTroopAmount(enemyAmount, enemyType, false);
+            }
+
+            Destroy(gameObject);
+        }
     }
 
-    void setHealth(int health)
+    // Change Speed (Based on Troop Type)
+    void SetSpeed()
     {
-        this.health = health;
+        if (enemyType == "F")
+        {
+            speed = 0.2f;
+        }
+        else if (enemyType == "S")
+        {
+            speed = 0.1f;
+        }
     }
 
-    int getHealth()
+    GameObject TargetTower()
     {
-        return health;
-    }
-
-    void setDamage(int damage)
-    {
-        this.damage = damage;
-    }
-
-    int getDamage()
-    {
-        return damage;
-    }
-
-    void setType(string type)
-    {
-        this.type = type;
-    }
-
-    string getType()
-    {
-        return type; ;
-    }
-
-    void setSpeed(float speed)
-    {
-        this.speed = speed;
-    }
-
-    float getSpeed()
-    {
-        return speed;
+        for (int i = 0; i < Tower.transform.childCount; i++)
+        {
+            GameObject currentTower = Tower.transform.GetChild(i).gameObject;
+            if (currentTower.tag == "Blue" || currentTower.tag == "white")
+            {
+                return currentTower;
+                break;
+            }
+        }
     }
 }
