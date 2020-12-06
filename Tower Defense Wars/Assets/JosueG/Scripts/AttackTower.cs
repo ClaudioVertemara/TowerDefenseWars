5ebef4f7a624ba20b5df77b4eb70f927667c7563
Josue-Gonzalez-Juan <josgonzalez@csumb.edu>
2020-12-06 15:04:33 -0800

    Ignoring Allies

--- Tower Defense Wars/Assets/JosueG/Scripts/AttackTower.cs
+++ Tower Defense Wars/Assets/JosueG/Scripts/AttackTower.cs
@@ -8,17 +8,23 @@ public class AttackTower : MonoBehaviour
 
     public float fireRate;
 
+    bool shooting;
+
+    public int damage;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyTroops = new List<GameObject>();
-        fireRate = 0.05f;
+        fireRate = 1.5f;
+        damage = 1;
+        shooting = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enemyTroops.Count != 0)
+        if (enemyTroops.Count != 0 && !shooting)
         {
             StartCoroutine(fire(fireRate));
         }
@@ -27,12 +33,24 @@ public class AttackTower : MonoBehaviour
 
     private IEnumerator fire(float waitTime)
     {
-        yield return new WaitForSeconds(waitTime);
+        shooting = true;
         Debug.DrawLine(transform.position, enemyTroops[0].transform.position, Color.white, 0.1f);
+        enemyTroops[0].GetComponent<Troops>().takeDamage(damage);
+        if(enemyTroops[0] == null)
+        {
+            enemyTroops.Remove(enemyTroops[0]);
+        }
+        yield return new WaitForSeconds(waitTime);
+        shooting = false;
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        //if the troop is from the same 
+        if (col.gameObject.CompareTag(gameObject.tag))
+        {
+            return;
+        }
         enemyTroops.Add(col.gameObject);
     }
 
